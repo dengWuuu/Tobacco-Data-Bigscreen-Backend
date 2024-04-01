2bab8e202f72423d727f10513b3c192bf58476a3
Jackson_Wu <dengwu.wu@foxmail.com>
2024-04-01 20:19:13 +0800

    feat: 获取所有商品接口添加

--- src/main/java/cn/com/v2/controller/TobaccoSpuController.java
+++ src/main/java/cn/com/v2/controller/TobaccoSpuController.java
@@ -1,12 +1,20 @@
 package cn.com.v2.controller;
 
 import cn.com.v2.common.config.V2Config;
+import cn.com.v2.common.domain.AjaxResult;
+import cn.com.v2.model.TobaccoSpu;
 import cn.com.v2.service.TobaccoSpuService;
 import io.swagger.annotations.Api;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import static cn.com.v2.common.domain.AjaxResult.error;
+import static cn.com.v2.common.domain.AjaxResult.success;
 
 /**
  * @author Wu
@@ -21,5 +29,15 @@ public class TobaccoSpuController {
     private V2Config v2Config;
     @Autowired
     private TobaccoSpuService tobaccoSpuService;
+
+    // 获取所有商品
+    @GetMapping("/list")
+    public AjaxResult list() {
+        List<TobaccoSpu> list = tobaccoSpuService.list();
+        if (list != null) {
+            return success().put("data", list);
+        }
+        return error();
+    }
 
 }
